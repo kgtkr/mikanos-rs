825474ef03ee07afdcd1fbc60759e6b75829a811
kgtkr <kgtkr.jp@gmail.com>
2021-04-04 16:48:24 +0900

    コードリファクタリング

--- uefi/src/boot_services.rs
+++ uefi/src/boot_services.rs
@@ -21,15 +21,15 @@ pub struct MemoryDescriptor {
 pub struct BootServices {
     hdr: TableHeader,
     dummy1: [usize; 4],
-    get_memory_map: extern "efiapi" fn(
+    get_memory_map: unsafe extern "efiapi" fn(
         memory_map_size: NonNull<usize>,
-        memory_map: NonNull<u8>,
-        map_key: NonNull<usize>,
-        descriptor_size: NonNull<usize>,
-        descriptor_version: NonNull<u32>,
+        memory_map: NonNull<[MaybeUninit<u8>]>,
+        map_key: NonNull<MaybeUninit<usize>>,
+        descriptor_size: NonNull<MaybeUninit<usize>>,
+        descriptor_version: NonNull<MaybeUninit<u32>>,
     ) -> Status,
     dummy2: [usize; 27],
-    open_protocol: extern "efiapi" fn(
+    open_protocol: unsafe extern "efiapi" fn(
         handle: Handle,
         protocol: NonNull<GUID>,
         interface: NonNull<NonNull<c_void>>,
@@ -44,7 +44,7 @@ pub struct BootServices {
 pub struct MemoryMap<'a> {
     map_key: usize,
     memory_map_size: usize,
-    memory_map: NonNull<u8>,
+    memory_map: NonNull<MaybeUninit<MemoryDescriptor>>,
     descriptor_size: usize,
     descriptor_version: u32,
     phantom: PhantomData<&'a ()>,
@@ -71,19 +71,17 @@ impl<'a> MemoryMap<'a> {
     }
 
     pub fn iter(&self) -> MemoryMapIter<'a> {
-        unsafe {
-            MemoryMapIter {
-                ptr: NonNull::new_unchecked(self.memory_map.as_ptr() as _),
-                len: self.len(),
-                descriptor_size: self.descriptor_size,
-                phantom: PhantomData,
-            }
+        MemoryMapIter {
+            ptr: self.memory_map,
+            len: self.len(),
+            descriptor_size: self.descriptor_size,
+            phantom: PhantomData,
         }
     }
 }
 
 pub struct MemoryMapIter<'a> {
-    ptr: NonNull<MemoryDescriptor>,
+    ptr: NonNull<MaybeUninit<MemoryDescriptor>>,
     len: usize,
     descriptor_size: usize,
     phantom: PhantomData<&'a ()>,
@@ -97,10 +95,10 @@ impl<'a> Iterator for MemoryMapIter<'a> {
             None
         } else {
             unsafe {
-                let result = &*self.ptr.as_ptr();
+                let result = &*(self.ptr.as_ptr() as *const MemoryDescriptor);
                 self.len -= 1;
                 self.ptr = NonNull::new_unchecked(
-                    (self.ptr.as_ptr() as usize + self.descriptor_size) as *mut MemoryDescriptor,
+                    (self.ptr.as_ptr() as usize + self.descriptor_size) as *mut _,
                 );
                 Some(result)
             }
@@ -123,21 +121,19 @@ impl BootServices {
         let mut descriptor_version = MaybeUninit::<u32>::uninit();
 
         unsafe {
-            let memory_map_ptr = NonNull::new_unchecked(memory_map.as_mut_ptr() as *mut u8);
-
             let status = (self.get_memory_map)(
-                NonNull::new_unchecked(&mut memory_map_size as *mut _),
-                memory_map_ptr,
-                NonNull::new_unchecked(map_key.as_mut_ptr()),
-                NonNull::new_unchecked(descriptor_size.as_mut_ptr()),
-                NonNull::new_unchecked(descriptor_version.as_mut_ptr()),
+                (&mut memory_map_size).into(),
+                memory_map.into(),
+                (&mut map_key).into(),
+                (&mut descriptor_size).into(),
+                (&mut descriptor_version).into(),
             );
 
             match status {
                 Status::Success => Ok(MemoryMap {
                     map_key: map_key.assume_init(),
                     memory_map_size: memory_map_size,
-                    memory_map: memory_map_ptr,
+                    memory_map: NonNull::<_>::from(memory_map).cast::<_>(),
                     descriptor_size: descriptor_size.assume_init(),
                     descriptor_version: descriptor_version.assume_init(),
                     phantom: PhantomData,
